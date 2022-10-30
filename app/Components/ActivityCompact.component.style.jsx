44b5dd4602df90806011784a3cf57ac61eb5ac08
Nora Salehson <wn00202145@wgs.wuerth.com>
2022-10-30 10:41:52 +0100

    Added 3d playback stats

--- app/Components/ActivityCompact.component.style.jsx
+++ app/Components/ActivityCompact.component.style.jsx
@@ -103,7 +103,6 @@ export default class ActivityCompactStyle {
                 paddingLeft: 80,
 
                 item: {
-
                     justifyContent: "space-between",
                     alignItems: "center",
 
