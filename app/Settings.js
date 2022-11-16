44df6224fa570467f69d4c81ecb5f37a2f19e25c
Nora Salehson <wn00202145@wgs.wuerth.com>
2022-11-16 19:04:30 +0100

    Added settings to gitignore

--- app/Settings.js
+++
@@ -1,5 +0,0 @@
-export default class config {
-    static api = "https://api.ridetracker.app";
-    
-    static production =  "public";
-};
---
+++ app/Settings.template.js
@@ -0,0 +1,5 @@
+export default class config {
+    static api = "";
+    
+    static production = "";
+};
