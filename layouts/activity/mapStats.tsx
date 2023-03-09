af635c80bdfa7cd53c9db4746d762a772278ae59
Nora Söderlund <norasoderlund@icloud.com>
2023-03-09 17:06:31 +0100

    Added map stats overlay

--- layouts/activity/mapStats.tsx
+++ layouts/activity/mapStats.tsx
@@ -1,33 +1,43 @@
-import { Text, View } from "react-native"
+import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function ActivityMapStats({ activity }) {
     return (
-        <View style={{
+        <LinearGradient 
+            colors={[ "transparent", "rgba(0, 0, 0, .4)" ]}
+            locations={[ 0.5, 1 ]}
+            style={{
             position: "absolute",
 
-            bottom: 8,
             right: 0,
 
-            width: "80%",
+            width: "100%",
             paddingRight: "5%",
+            paddingLeft: "20%",
+            paddingBottom: 8,
+            height: "100%",
+
+            borderRadius: 10,
+
+            alignItems: "flex-end",
 
             flexDirection: "row",
             justifyContent: "space-between"
         }}>
             <View>
-                <Text style={{ textShadowColor: "#FFF", textShadowRadius: 2, textAlign: "center", fontSize: 28, fontWeight: "500" }}>7.3<Text style={{ fontSize: 18 }}> km</Text></Text>
-                <Text style={{ textShadowColor: "#FFF", textShadowRadius: 2, textAlign: "center", fontSize: 16 }}>distance</Text>
+                <Text style={{ color: "#FFF", textAlign: "center", fontSize: 28, fontWeight: "500" }}>7.3<Text style={{ fontSize: 18 }}> km</Text></Text>
+                <Text style={{ color: "#FFF", textAlign: "center", fontSize: 16 }}>distance</Text>
             </View>
             
             <View>
-                <Text style={{ textShadowColor: "#FFF", textShadowRadius: 2, textAlign: "center", fontSize: 28, fontWeight: "500" }}>23.7<Text style={{ fontSize: 18 }}> km/h</Text></Text>
-                <Text style={{ textShadowColor: "#FFF", textShadowRadius: 2, textAlign: "center", fontSize: 16 }}>average speed</Text>
+                <Text style={{ color: "#FFF", textAlign: "center", fontSize: 28, fontWeight: "500" }}>23.7<Text style={{ fontSize: 18 }}> km/h</Text></Text>
+                <Text style={{ color: "#FFF", textAlign: "center", fontSize: 16 }}>average speed</Text>
             </View>
             
             <View>
-                <Text style={{ textShadowColor: "#FFF", textShadowRadius: 2, textAlign: "center", fontSize: 28, fontWeight: "500" }}>33<Text style={{ fontSize: 18 }}> m</Text></Text>
-                <Text style={{ textShadowColor: "#FFF", textShadowRadius: 2, textAlign: "center", fontSize: 16 }}>elevation</Text>
+                <Text style={{ color: "#FFF", textAlign: "center", fontSize: 28, fontWeight: "500" }}>33<Text style={{ fontSize: 18 }}> m</Text></Text>
+                <Text style={{ color: "#FFF", textAlign: "center", fontSize: 16 }}>elevation</Text>
             </View>
-        </View>
+        </LinearGradient>
     );
 };
