beffa96adf8b246bd6e843233bb161d3139bbef9
Nora Söderlund <nora.soderlund@polestar.com>
2023-04-27 09:34:00 +0200

    Update stats styling

--- layouts/activity/stats.tsx
+++ layouts/activity/stats.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Text, View } from "react-native";
 import { useTheme } from "../../utils/themes";
 import { ActivityPersonalBest } from "../../components/ActivityPersonalBest";
+import { ParagraphText } from "../../components/texts/paragraph";
 
 type ActivityStatsProps = {
     activity: any | null;
@@ -46,42 +47,50 @@ export default function ActivityStats({ activity }: ActivityStatsProps) {
     return (
         <View style={{ gap: 20, padding: 20 }}>
             <View style={{ flexDirection: "row" }}>
-                <View style={{ width: "50%" }}>
-                    <View style={{ marginLeft: "auto", marginRight: "auto", position: "relative" }}>
+                <View style={{ width: "50%", gap: 5 }}>
+                    <View style={{ marginLeft: "auto", marginRight: "auto", position: "relative", paddingHorizontal: 10 }}>
                         {(!!activity.summary?.distancePersonalBest) && (<ActivityPersonalBest/>)}
 
                         <Text style={{ color: theme.color, textAlign: "center", fontSize: 28, fontWeight: "500" }}>{activity.summary.distance} km</Text>
                         <Text style={{ color: theme.color, textAlign: "center", fontSize: 16 }}>distance</Text>
                     </View>
+                    
+                    {(!!activity.summary?.distancePersonalBest) && (<ParagraphText style={{ textAlign: "center" }}>Personal Best</ParagraphText>)}
                 </View>
                 
-                <View style={{ width: "50%" }}>
-                    <View style={{ marginLeft: "auto", marginRight: "auto", position: "relative" }}>
+                <View style={{ width: "50%", gap: 5 }}>
+                    <View style={{ marginLeft: "auto", marginRight: "auto", position: "relative", paddingHorizontal: 10 }}>
                         {(!!activity.summary?.averageSpeedPersonalBest) && (<ActivityPersonalBest/>)}
 
                         <Text style={{ color: theme.color, textAlign: "center", fontSize: 28, fontWeight: "500" }}>{activity.summary.averageSpeed} km/h</Text>
-                        <Text style={{ color: theme.color, textAlign: "center", fontSize: 16 }}>average speed</Text>
+                        <Text style={{ color: theme.color, textAlign: "center", fontSize: 16 }}>{(!!activity.summary?.averageSpeedPersonalBest)?("avg.speed"):("average speed")}</Text>
                     </View>
+                    
+                    {(!!activity.summary?.averageSpeedPersonalBest) && (<ParagraphText style={{ textAlign: "center" }}>Personal Best</ParagraphText>)}
                 </View>
             </View>
             
             <View style={{ flexDirection: "row" }}>
-                <View style={{ width: "50%" }}>
-                    <View style={{ marginLeft: "auto", marginRight: "auto", position: "relative" }}>
+                <View style={{ width: "50%", gap: 5 }}>
+                    <View style={{ marginLeft: "auto", marginRight: "auto", position: "relative", paddingHorizontal: 10 }}>
                         {(!!activity.summary?.elevationPersonalBest) && (<ActivityPersonalBest/>)}
 
                         <Text style={{ color: theme.color, textAlign: "center", fontSize: 28, fontWeight: "500" }}>{activity.summary.elevation} m</Text>
                         <Text style={{ color: theme.color, textAlign: "center", fontSize: 16 }}>elevation</Text>
                     </View>
+                    
+                    {(!!activity.summary?.elevationPersonalBest) && (<ParagraphText style={{ textAlign: "center" }}>Personal Best</ParagraphText>)}
                 </View>
                 
-                <View style={{ width: "50%" }}>
-                    <View style={{ marginLeft: "auto", marginRight: "auto", position: "relative" }}>
+                <View style={{ width: "50%", gap: 5 }}>
+                    <View style={{ marginLeft: "auto", marginRight: "auto", position: "relative", paddingHorizontal: 10 }}>
                         {(!!activity.summary?.maxSpeedPersonalBest) && (<ActivityPersonalBest/>)}
 
                         <Text style={{ color: theme.color, textAlign: "center", fontSize: 28, fontWeight: "500" }}>{activity.summary.maxSpeed} km/h</Text>
                         <Text style={{ color: theme.color, textAlign: "center", fontSize: 16 }}>max speed</Text>
                     </View>
+                    
+                    {(!!activity.summary?.maxSpeedPersonalBest) && (<ParagraphText style={{ textAlign: "center" }}>Personal Best</ParagraphText>)}
                 </View>
             </View>
         </View>
